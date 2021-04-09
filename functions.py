9c9df8973a1402e2a8879bae0963b78281eb5d04
Patrick Pang <mail@patrickpang.net>
2021-04-10 00:07:18 +1000

    Added support for live photos. Refactored functions and classes.

--- functions.py
+++ functions.py
@@ -1,14 +1,44 @@
 import hashlib
+import json
+import sys
+from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
+from typing import Optional
+
+
+@dataclass
+class Account:
+    username: str
+    dest: str
+    album: Optional[str]
+    repeat_limit: int
+
+
+@dataclass()
+class DownloadTask:
+    photo_id: str
+    create_date: datetime
+    cloud_filename: str
+    url: str
+    suffix: str = ""
+
+    def get_path(self, dest: Path) -> Path:
+        photo_sha1_id = sha1_id(self.photo_id)
+        date_part = self.create_date.strftime("%Y%m%d_%H%M%S")
+        ext = Path(self.cloud_filename).suffix.lower()
+        return dest / f"{date_part}_{photo_sha1_id}{self.suffix}{ext}"
 
 
 def sha1_id(s: str, length=7) -> str:
     return hashlib.sha1(s.encode()).hexdigest()[:length]
 
 
-def get_path(photo_id: str, create_date: datetime, filename: str, dest: Path, suffix="") -> Path:
-    photo_sha1_id = sha1_id(photo_id)
-    date_part = create_date.strftime("%Y%m%d_%H%M%S")
-    ext = Path(filename).suffix.lower()
-    return dest / f"{date_part}_{photo_sha1_id}{suffix}{ext}"
+def get_account() -> Account:
+    if len(sys.argv) < 2 or not sys.argv[1].isnumeric():
+        raise Exception(f"Please specify a valid account index.")
+    selected_account = int(sys.argv[1])
+    with open("accounts.json", "r") as accounts_fp:
+        account = json.load(accounts_fp)[selected_account]
+        return Account(username=account["username"], dest=account["dest"], album=account["album"] or None,
+                       repeat_limit=int(account["repeat_limit"]))
--- icbackup.py
+++ icbackup.py
@@ -1,48 +1,65 @@
-import json
-import sys
 from pathlib import Path
+from typing import List
 
 import requests
 from pyicloud import PyiCloudService
 from tqdm import tqdm
 
-from functions import get_path
+from functions import get_account, DownloadTask
 
-try:
-    selected_account = int(sys.argv[1])
-except ValueError:
-    print(f"Invalid account index {sys.argv[1]}.")
-    sys.exit(1)
-except IndexError:
-    print(f"Please specify account index.")
-    sys.exit(2)
+account = get_account()
+api = PyiCloudService(account.username)
+if not api.is_trusted_session:
+    raise Exception("Session has not trusted. Please run icloud --username (email) to authenticate.")
 
-with open("accounts.json", "r") as accounts_fp:
-    account = json.load(accounts_fp)[selected_account]
-
-api = PyiCloudService(account["username"])
-album = api.photos.albums[account["album"]] if account["album"] else api.photos.all
-dest = Path(account["dest"]).resolve()
+album = api.photos.albums[account.album] if account.album else api.photos.all
+dest = Path(account.dest).resolve()
 
 session = requests.Session()
-for photo in tqdm(album.photos, total=len(album), desc=account["username"], unit="p"):
+repeat_count = 0
+for photo in tqdm(album.photos, total=len(album), desc=account.username, unit="p"):
+    if repeat_count >= account.repeat_limit:
+        print(f"Found {repeat_count} repeated images. Download stopped.")
+        break
+
+    download_list: List[DownloadTask] = []
     try:
-        filename = photo._asset_record["fields"]["resJPEGFullFileType"]["value"]    # "public.heic" or "public.jpeg"
-        photo_dest_path = get_path(photo.id, photo.created, filename, dest, "_E")
-        url = photo._asset_record["fields"]["resJPEGFullRes"]["value"]["downloadURL"]
+        full_type = photo._asset_record["fields"]["resJPEGFullFileType"]["value"]    # "public.heic" or "public.jpeg"
+        if full_type == "public.heic":
+            filename = "photo.heic"
+        elif full_type == "public.jpeg":
+            filename = "photo.jpg"
+        else:
+            raise RuntimeError(f"Unknown full JPEG type for {photo}: {full_type}.")
+        t = DownloadTask(photo.id, photo.created, filename,
+                         photo._asset_record["fields"]["resJPEGFullRes"]["value"]["downloadURL"], suffix="_E")
     except KeyError:
-        photo_dest_path = get_path(photo.id, photo.created, photo.filename, dest)
-        url = photo.versions["original"]["url"]
+        t = DownloadTask(photo.id, photo.created, photo.filename, photo.versions["original"]["url"])
+    download_list.append(t)
 
-    if photo_dest_path.exists():
-        continue
+    # Live photos
+    try:
+        if photo._master_record["fields"]["resOriginalVidComplFileType"]["value"] == "com.apple.quicktime-movie":
+            t = DownloadTask(photo.id, photo.created, "live_video.mov",
+                             photo._master_record["fields"]["resOriginalVidComplRes"]["value"]["downloadURL"])
+            download_list.append(t)
+            for t in download_list:
+                t.suffix = "_LP"
+    except KeyError:
+        pass
 
-    with session.get(url, stream=True) as req:
-        req.raise_for_status()
-        with photo_dest_path.open("wb") as fp:
-            pbar = tqdm(total=int(req.headers["Content-Length"]), desc=photo_dest_path.name,
+    for task in download_list:
+        photo_dest_path = task.get_path(dest)
+        if photo_dest_path.exists():
+            repeat_count += 1
+            continue
+
+        repeat_count = 0  # Reset repeat count if file does not exist.
+        with session.get(task.url, stream=True) as req:
+            req.raise_for_status()
+            pbar = tqdm(total=int(req.headers["Content-Length"]), desc=photo_dest_path.name, leave=False,
                         bar_format="{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}")
-            for chunk in req.iter_content(32768):
-                fp.write(chunk)
-                pbar.update(len(chunk))
-            pbar.close()
+            with photo_dest_path.open("wb") as fp, pbar:
+                for chunk in req.iter_content(32768):
+                    fp.write(chunk)
+                    pbar.update(len(chunk))
